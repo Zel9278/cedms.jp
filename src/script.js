3d628a7b93033c82510e92d6de3204cadb364d92
ced <ced@ced.ced>
2021-04-28 11:48:50 +0900

    I RESTONE WEBPAGE!

---
+++ src/script.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const ws = new WebSocket("wss://mpp.cedms.jp:8443");
+    const ws_img = document.getElementById("ws");
+    ws.onopen= function (e) {
+        ws_img.src = "src/images/icons/ws/check.png";
+    }    
+    ws.onclose = function (e) {
+        ws_img.src = "src/images/icons/ws/close.png";
+    }
+});
