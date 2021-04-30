30803c713390376b586594b784d554433a531c89
ced <ced@ced.ced>
2021-05-01 06:53:42 +0900

    update homepage script

--- src/script.js
+++ src/script.js
@@ -1,10 +1,23 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
+    wsChecker();
+});
+
+function wsChecker() {
     const ws = new WebSocket("wss://mpp.cedms.jp:8443");
     const ws_img = document.getElementById("ws");
-    ws.onopen= function (e) {
+    ws_img.src = "src/images/icons/ws/wait.png";
+    ws.onopen = (e) => {
         ws_img.src = "src/images/icons/ws/check.png";
     }    
-    ws.onclose = function (e) {
+    ws.onclose = (e) => {
         ws_img.src = "src/images/icons/ws/close.png";
+        console.warn("websocket has been disconnected for some reason, will reconnect in a second...");
+        setTimeout(() => {
+            wsChecker();
+        }, 1000);
     }
-});
+    ws.onerror = (e) => {
+        ws_img.src = "src/images/icons/ws/close.png";
+        ws.close();
+    }
+}
